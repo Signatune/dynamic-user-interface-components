05966a37c39570ab33ea627b53006f3a034271e3
Mike <michaelambrosino93@gmail.com>
2023-10-17 23:50:48 -0400

    dropdown mvp

---
+++ src/DropDownMenu.js
@@ -0,0 +1,48 @@
+import "./style.css";
+
+function DropDownMenu() {
+  const options = ["One", "Two", "Three", "Four", "Five", "Six"];
+  let selected = "Select...";
+
+  const menuEl = document.createElement("div");
+  menuEl.classList.add("menu");
+
+  function setSelected(id) {
+    selected = options[id];
+  }
+
+  function render() {
+    menuEl.innerHTML = `
+      <div class="selected">${selected}</div>
+      <ul class="options">
+        ${options
+          .map((option, index) => {
+            if (option !== selected) {
+              return `<li id="${index}" class="option">${option}</li>`;
+            }
+
+            return `<li id="${index} class="option">${option} ✓</li>`;
+          })
+          .join("")}
+      </ul>
+    `;
+
+    menuEl.querySelector(".selected").addEventListener("click", () => {
+      menuEl.querySelector(".options").classList.add("visible");
+    });
+
+    menuEl.querySelectorAll(".option").forEach((option) => {
+      option.addEventListener("click", () => {
+        setSelected(option.id);
+        render();
+      });
+    });
+  }
+
+  return {
+    menuEl,
+    render,
+  };
+}
+
+export default DropDownMenu;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import DropDownMenu from "./DropDownMenu.js";
 
+const menu = DropDownMenu();
+menu.render();
+
+document.body.appendChild(menu.menuEl);
